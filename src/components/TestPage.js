61eb101374794cc0f837aa877d3cf26970197a28
Dikshit <singhdikshit69@gmail.com>
2023-09-25 23:06:17 +0530

    fix(TestPage.js): fix import statements to use double quotes instead of single quotes for better consistency feat(TestPage.js): add logic to fetch test questions from API based on question ID feat(TestPage.js): add timer functionality to track total time for the test feat(TestPage.js): add logic to handle next and previous questions feat(TestPage.js): add logic to submit test results and display alert message feat(TestPage.js): add MathJax rendering for test questions feat(TestPage.js): add button to submit test and display message for last question

--- src/components/TestPage.js
+++ src/components/TestPage.js
@@ -1,46 +1,44 @@
-import { MathJax, MathJaxContext } from 'better-react-mathjax';
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { addIndex, addQuestionTimer, addTotalTime } from '../redux/TestPageSlice';
-
+import { MathJax, MathJaxContext } from "better-react-mathjax";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  addIndex,
+  addQuestionTimer,
+  addTotalTime,
+} from "../redux/TestPageSlice";
 
 function TestPage() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const questions =useSelector(store=>store.landingpage.questions)
-  const [testQuestions,setTestQuestions]=useState([])
-  const[totalTime,setTotalTime]=useState(0);
-
-
+  const questions = useSelector((store) => store.landingpage.questions);
+  const [testQuestions, setTestQuestions] = useState("");
+  const [totalTime, setTotalTime] = useState(0);
 
   // total time for the test
-useEffect(() => {
-  const intervalTotal = setInterval(() => {
-    setTotalTime((prevTimer) => prevTimer + 1);
-  }, 1000); // Update the timer every second
+  useEffect(() => {
+    const intervalTotal = setInterval(() => {
+      setTotalTime((prevTimer) => prevTimer + 1);
+    }, 1000); // Update the timer every second
 
-  // Clear the interval when the component unmounts
-  return () => clearInterval(intervalTotal);
-}, []);
-  
-  const getQuestions=async(id)=>{
+    // Clear the interval when the component unmounts
+    return () => clearInterval(intervalTotal);
+  }, []);
+
+  const getQuestions = async (id) => {
     try {
-      const data=await fetch(`https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails?QuestionID=${id}`)
-      const json= await data.json()
+      const data = await fetch(
+        `https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails?QuestionID=${id}`
+      );
+      const json = await data.json();
       setTestQuestions(json[0].Question);
-      
     } catch (error) {
-      console.log('Eror while fetching data',error);
+      console.log("Eror while fetching data", error);
     }
-  }
+  };
 
-
-  useEffect(()=>{
-    getQuestions(questions[currentQuestionIndex])
-  },[currentQuestionIndex])
-
-
-
+  useEffect(() => {
+    getQuestions(questions[currentQuestionIndex]);
+  }, [currentQuestionIndex]);
 
   const [timer, setTimer] = useState(0); // Timer in seconds
 
@@ -49,7 +47,9 @@ useEffect(() => {
     const hours = Math.floor(timeInSeconds / 3600);
     const minutes = Math.floor((timeInSeconds % 3600) / 60);
     const seconds = timeInSeconds % 60;
-    return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
   };
 
   useEffect(() => {
@@ -62,105 +62,100 @@ useEffect(() => {
     return () => clearInterval(interval);
   }, []);
 
-
-
-
-
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
 
   const handleNextQuestion = () => {
-    // Move to the next question if available
-    if (currentQuestionIndex < questions.length) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-      dispatch(addQuestionTimer(timer))
-      dispatch(addIndex(currentQuestionIndex+1));
-      setTimer(0);
+    // Disable the button if the current question is already the last question
+    if (currentQuestionIndex === questions.length - 1) {
+      return;
     }
-  };
 
+    // Move to the next question if available
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+    dispatch(addQuestionTimer(timer));
+    dispatch(addIndex(currentQuestionIndex + 1));
+    setTimer(0);
+  };
 
   const handlePreviousQuestion = () => {
     // Move to the previous question if available
     if (currentQuestionIndex > 0) {
       setCurrentQuestionIndex(currentQuestionIndex - 1);
-      dispatch(addIndex(currentQuestionIndex-1));
-      setTimer(0)
+      dispatch(addIndex(currentQuestionIndex - 1));
+      setTimer(0);
     }
   };
 
   const handleSubmitTest = () => {
     // Add logic to submit the test results
-    alert('Test submitted!'); // Example alert for demonstration
+    alert("Test submitted!"); // Example alert for demonstration
 
-    dispatch(addQuestionTimer(timer)) //for last question
-    dispatch(addTotalTime(totalTime))
+    dispatch(addQuestionTimer(timer)); //for last question
+    dispatch(addTotalTime(totalTime));
   };
-
-
-
-
+  
   const config = {
     loader: { load: ["input/asciimath"] },
     asciimath: {
-      delimiters: [['$','$'], ['`','`']]
+      delimiters: [
+        ["$", "$"],
+        ["`", "`"],
+        ["$", "$"],
+        ["\\\\(", "\\\\)"],
+      ],
     },
-};
-
-
-
-
-
-
-
+  };
 
   return (
     <MathJaxContext config={config}>
-    <div className="py-8 my-16">
+      <div className="py-8 my-16">
+        <div className="w-full h-full mx-auto bg-white p-6 rounded shadow-lg">
+          <div className=" flex justify-end mb-4">
+            <span className="text-lg font-semibold">
+              Timer: {formatTime(timer)}
+            </span>
+          </div>
 
-      <div className="w-full h-full mx-auto bg-white p-6 rounded shadow-lg">
+          <div className="mb-4">
+            <p className="text-lg font-semibold overflow-x-scroll">
+              <span className="text-xl text-red-700">{`Question ${
+                currentQuestionIndex + 1
+              } : `}</span>
+              {testQuestions && <MathJax>{testQuestions}</MathJax>}
+            </p>
+          </div>
 
-        <div className=" flex justify-end mb-4">
-          <span className="text-lg font-semibold">Timer: {formatTime(timer)}</span>
+          <div className="flex justify-between w-full  my-8">
+            <button
+              className="max-sm:px-2 max-sm:py-1 bg-blue-500 text-white px-4 py-2 rounded mr-2"
+              onClick={handlePreviousQuestion}
+              disabled={currentQuestionIndex === 0}
+            >
+              Previous
+            </button>
+            <button
+              className="bg-blue-500 max-sm:px-2 max-sm:py-1 text-white px-4 py-2 rounded"
+              onClick={handleNextQuestion}
+              disabled={currentQuestionIndex === questions.length - 1}
+            >
+              Next Question
+            </button>
+          </div>
+
+          <div className="flex max-sm:flex-col-reverse justify-between mb-4">
+            <p className="text-red-600 max-sm:mt-4">
+              * If it is the last question then click on submit test
+            </p>
+            <Link
+              to="/finish"
+              className="max-sm:px-2 max-sm:py-1 bg-red-700 max-sm:w-[105px] text-white px-4 py-2 rounded"
+              onClick={handleSubmitTest}
+            >
+              Submit Test
+            </Link>
+          </div>
         </div>
-
-      
-        <div className="mb-4">
-          <p className="text-lg font-semibold overflow-x-scroll"><span className='text-xl text-red-700'>{`Question ${currentQuestionIndex+1} : `}</span><MathJax>{testQuestions}</MathJax></p>
-        </div>
-        
-
-
-        <div className="flex justify-between w-full  my-8">
-          <button
-            className="max-sm:px-2 max-sm:py-1 bg-blue-500 text-white px-4 py-2 rounded mr-2"
-            onClick={handlePreviousQuestion}
-            disabled={currentQuestionIndex === 0}
-          >
-            Previous
-          </button>
-          <button
-            className="bg-blue-500 max-sm:px-2 max-sm:py-1 text-white px-4 py-2 rounded"
-            onClick={handleNextQuestion}
-            disabled={currentQuestionIndex === questions.length - 1}
-          >
-            Next Question
-          </button>
-        </div>
-
-
-
-       <div  className='flex max-sm:flex-col-reverse justify-between mb-4' >
-        
-        <p className='text-red-600 max-sm:mt-4'>* If it is the last question then click on submit test</p>
-        <Link to="/finish" className= "max-sm:px-2 max-sm:py-1 bg-red-700 max-sm:w-[105px] text-white px-4 py-2 rounded" onClick={handleSubmitTest}>
-            Submit Test
-        </Link>
-</div>
-
       </div>
-
-
-    </div>
     </MathJaxContext>
   );
 }
